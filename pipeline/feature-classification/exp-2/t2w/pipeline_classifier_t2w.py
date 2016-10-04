9c3ba4f3b2fe5bdc8819580ce942a5815162b33c
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-10-04 15:36:15 +0200

    Add the classification with balancing for t2w

--- pipeline/feature-classification/exp-2/t2w/pipeline_classifier_t2w.py
+++ pipeline/feature-classification/exp-2/t2w/pipeline_classifier_t2w.py
@@ -18,18 +18,20 @@ path_features = '/data/prostate/extraction/mp-mri-prostate'
 # Define a list of the path where the feature are kept
 t2w_features = ['dct-t2w', 'edge-t2w/kirsch', 'edge-t2w/laplacian',
                 'edge-t2w/prewitt', 'edge-t2w/scharr', 'edge-t2w/sobel',
-                'gabor-t2w', 'harlick-t2w', 'ise-t2w', 'phase-congruency-t2w',
-                'spatial-position-euclidean', 'spatial-dist-center',
-                'spatial-dist-contour']
+                'gabor-t2w', 'harlick-t2w', 'ise-t2w', 'lbp-t2w', 'lbp-t2w',
+                'phase-congruency-t2w']#,
+#                'spatial-position-euclidean', 'spatial-dist-center',
+#                'spatial-dist-contour']
 # Define the extension of each features
 ext_features = ['_dct_t2w.npy', '_edge_t2w.npy', '_edge_t2w.npy',
                 '_edge_t2w.npy', '_edge_t2w.npy', '_edge_t2w.npy',
                 '_gabor_t2w.npy', '_haralick_t2w.npy', '_ise_t2w.npy',
-                '_phase_congruency_t2w.npy', '_spe.npy', '_spe.npy',
-                '_spe.npy']
+                '_lbp_8_1_t2w.npy', '_lbp_16_2_t2w.npy',
+                '_phase_congruency_t2w.npy']#, '_spe.npy', '_spe.npy',
+#                '_spe.npy']
 # Define the path of the balanced data
 path_balanced = '/data/prostate/balanced/mp-mri-prostate/exp-2'
-sub_folder = ['cc', 'iht', 'nm1', 'nm2', 'nm3', 'rus', 'adasyn', 'smote',
+sub_folder = ['iht', 'nm1', 'nm2', 'nm3', 'rus', 'adasyn', 'smote',
               'smote-b1', 'smote-b2', 'ros']
 ext_balanced = '_t2w.npz'
 # Define the path of the ground for the prostate
@@ -42,6 +44,7 @@ path_patients_list_gt = []
 # Create the generator
 id_patient_list = [name for name in os.listdir(path_patients)
                    if os.path.isdir(os.path.join(path_patients, name))]
+id_patient_list = sorted(id_patient_list)
 
 for id_patient in id_patient_list:
     # Append for the GT data - Note that we need a list of gt path
@@ -50,8 +53,11 @@ for id_patient in id_patient_list:
 
 # Load all the data once. Splitting into training and testing will be done at
 # the cross-validation time
+
 data = []
+data_bal = []
 label = []
+label_bal = []
 for idx_pat in range(len(id_patient_list)):
     print 'Read patient {}'.format(id_patient_list[idx_pat])
 
@@ -72,7 +78,24 @@ for idx_pat in range(len(id_patient_list)):
     # Concatenate the data in a single array
     patient_data = np.concatenate(patient_data, axis=1)
 
-    print 'Read the ADC data for the current patient ...'
+    print 'Imbalanced feature loaded ...'
+
+    # Load the dataset from each balancing method
+    data_bal_meth = []
+    label_bal_meth = []
+    for idx_imb in range(len(sub_folder)):
+        path_bal = os.path.join(path_balanced, sub_folder[idx_imb])
+        pat_chg = (id_patient_list[idx_pat].lower().replace(' ', '_') +
+               '_t2w.npz')
+        filename = os.path.join(path_bal, pat_chg)
+        npz_file = np.load(filename)
+        data_bal_meth.append(npz_file['data_resampled'])
+        label_bal_meth.append(npz_file['label_resampled'])
+
+    data_bal.append(data_bal_meth)
+    label_bal.append(label_bal_meth)
+
+    print 'Balanced data loaded ...'
 
     # Create the corresponding ground-truth
     gt_mod = GTModality()
@@ -90,40 +113,46 @@ for idx_pat in range(len(id_patient_list)):
     label.append(gt_cap[roi_prostate])
     print 'Data and label extracted for the current patient ...'
 
-result_cv = []
-# Go for LOPO cross-validation
-for idx_lopo_cv in range(len(id_patient_list)):
+results_bal = []
+for idx_imb in range(len(sub_folder)):
+    result_cv = []
+    # Go for LOPO cross-validation
+    for idx_lopo_cv in range(len(id_patient_list)):
 
-    # Display some information about the LOPO-CV
-    print 'Round #{} of the LOPO-CV'.format(idx_lopo_cv + 1)
+        # Display some information about the LOPO-CV
+        print 'Round #{} of the LOPO-CV'.format(idx_lopo_cv + 1)
 
-    # Get the testing data
-    testing_data = data[idx_lopo_cv]
-    testing_label = label_binarize(label[idx_lopo_cv], [0, 255])
-    print 'Create the testing set ...'
+        # Get the testing data
+        testing_data = data[idx_lopo_cv]
+        testing_label = label_binarize(label[idx_lopo_cv], [0, 255])
+        print 'Create the testing set ...'
 
-    # Create the training data and label
-    training_data = [arr for idx_arr, arr in enumerate(data)
-                     if idx_arr != idx_lopo_cv]
-    training_label = [arr for idx_arr, arr in enumerate(label)
-                     if idx_arr != idx_lopo_cv]
-    # Concatenate the data
-    training_data = np.vstack(training_data)
-    training_label = label_binarize(np.hstack(training_label).astype(int),
-                                    [0, 255])
-    print 'Create the training set ...'
+        # Create the training data and label
+        # We need to take the balanced data
+        training_data = [arr[idx_imb] for idx_arr, arr in enumerate(data_bal)
+                         if idx_arr != idx_lopo_cv]
+        training_label = [arr[idx_imb] for idx_arr, arr in enumerate(label_bal)
+                         if idx_arr != idx_lopo_cv]
+        # Concatenate the data
+        training_data = np.vstack(training_data)
+        training_label = label_binarize(np.hstack(training_label).astype(int),
+                                        [0, 255])
+        print 'Create the training set ...'
 
-    # Perform the classification for the current cv and the
-    # given configuration
-    crf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
-    pred_prob = crf.fit(training_data, np.ravel(training_label)).predict_proba(
-        testing_data)
+        # Perform the classification for the current cv and the
+        # given configuration
+        crf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+        pred_prob = crf.fit(training_data,
+                            np.ravel(training_label)).predict_proba(
+                                testing_data)
 
-    result_cv.append([pred_prob, crf.classes_])
+        result_cv.append([pred_prob, crf.classes_])
+
+    results_bal.append(result_cv)
 
 # Save the information
-path_store = '/data/prostate/results/mp-mri-prostate/exp-1-bis/t2w'
+path_store = '/data/prostate/results/mp-mri-prostate/exp-2/t2w'
 if not os.path.exists(path_store):
     os.makedirs(path_store)
-joblib.dump(result_cv, os.path.join(path_store,
-                                    'results.pkl'))
+joblib.dump(results_bal, os.path.join(path_store,
+                                     'results.pkl'))
